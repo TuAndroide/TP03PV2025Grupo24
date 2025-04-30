45f8b3ccbcc56c713011dcb92812b6aaed50f697
naty <natalisuarez440@gmail.com>
2025-04-29 22:00:51 -0300

    correccion punto 1,2 y 3 realizados por Natali Suarez

--- src/Adicional.jsx
+++ src/Adicional.jsx
@@ -1,17 +1,24 @@
 // App.jsx
 import { useEffect } from 'react';
-import productosOriginales from './productos.js';
 
-function App() {
+
+const productosOriginales = [
+  { descripcion: "Teclado", precio: 30000 },
+  { descripcion: "Auriculares", precio: 49000 },
+  { descripcion: "Mouse", precio: 15000 },
+  { descripcion: "Monitor", precio: 120000 },
+  { descripcion: "Webcam", precio: 25000 }
+];
+function Adicional() {
   useEffect(() => {
-    console.clear();9
+    console.clear(); 9
 
     // Copiamos el array original para no modificarlo directamente
     let productos = [...productosOriginales];
 
     console.log("1 - Lista de productos:");
     productos.forEach(p =>
-      console.log( `Producto: ${p.descripcion} - Precio: $${p.precio}`)
+      console.log(`Producto: ${p.descripcion} - Precio: $${p.precio}`)
     );
 
     console.log(`\n2 - Productos con precio mayor a $20:`);
@@ -26,11 +33,11 @@ function App() {
     console.log(productosConIVA);
   }, []);
 
-    return (
-      <div>
-        <h1>Ver consola para los resultados</h1>
-      </div>
-    );
+  return (
+    <div>
+      <h1>Ver consola para los resultados</h1>
+    </div>
+  );
 }
-  export default App;
+export default Adicional;
 
