d33fe21ec346d4dee886ba3d3d96c8a88f2d1a3d
GabrielCalisaya <gabrielcalisaya98@gmail.com>
2025-05-04 21:00:11 -0300

    Solucion de bug y errores en funcionalidades, implementacion de funciones para un funcionamiento correcto de ambos cuadros, conjunto una correcion del css que veniamos manejando anteriormente

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-import Productos from './Adicional.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import Productos from './Adicional.jsx';
+import './index.css'; // Importa el index
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
-    <Productos/>
+    <div className="app-container"> {/* Aplica la classe des de index.css */}
+      <App />
+      <Productos />
+    </div>
   </StrictMode>,
-)
+);
